b24c6ccc4feb3fd3a38b9db75155260b6b829698
rajurao <rajurao1107@gmail.com>
2025-03-22 14:01:41 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta
+      name="google-site-verification"
+      content="6rxbbidY7MSJ_tZTc2ZrkrT7fiAAo_WUw3mbp_gu9e4"
+    />
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>AI Guru - AI Learning Assistant</title>
@@ -39,10 +43,7 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="GvlapF8Ytg-lBlDMjHK9cDjpNMdic_GkB1Nv2X3g06g"
         />
-         <meta
-      name="google-site-verification"
-      content="6rxbbidY7MSJ_tZTc2ZrkrT7fiAAo_WUw3mbp_gu9e4"
-    />
+        
       </head>
       <body className={geist.className}>{children}</body>
     </html>
