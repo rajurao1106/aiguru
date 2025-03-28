9f70ba200ad2230e128f533a0cb5c303e32335d8
rajurao <rajurao1107@gmail.com>
2025-03-28 11:25:45 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const geist = Geist({ subsets: ["latin"], weight: "400" });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta charSet="UTF-8" /> {/* Moved to the first meta tag */}
         <meta
           name="google-site-verification"
@@ -25,10 +25,28 @@ export default function RootLayout({ children }) {
           name="keywords"
           content="ai in education, education in ai, ai on education, ai for education, artificial intelligence in education, ai and education, artificial intelligence and education, artificial intelligence on education, math problem solver"
         />
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D7K9P2R0NV"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-D7K9P2R0NV');
+          `,
+          }}
+        />
         <meta name="author" content="D. Raju Rao" />
         <meta name="robots" content="index, follow" />
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="AI Guru: Doubt Solver with AI for Education" />
+        <meta
+          property="og:title"
+          content="AI Guru: Doubt Solver with AI for Education"
+        />
         <meta
           property="og:description"
           content="Explore AI and education with our artificial intelligence in education platform! Solve doubts instantly using our math problem solver powered by artificial intelligence on education."
@@ -42,7 +60,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="AI Guru" />
-      </Head>
+      </head>
       <body className={geist.className}>{children}</body>
     </html>
   );
