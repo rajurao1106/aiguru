a142d65bf52cfefdee0a4a0eb56bfe402432a065
rajurao <rajurao1107@gmail.com>
2025-03-28 11:32:51 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,18 @@ export default function RootLayout({ children }) {
           name="keywords"
           content="ai in education, education in ai, ai on education, ai for education, artificial intelligence in education, ai and education, artificial intelligence and education, artificial intelligence on education, math problem solver"
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRSCH6XG');
+          `,
+          }}
+        />
         {/* Google Analytics */}
         <script
           async
@@ -61,7 +73,17 @@ export default function RootLayout({ children }) {
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="AI Guru" />
       </head>
-      <body className={geist.className}>{children}</body>
+      <body className={geist.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRSCH6XG"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
