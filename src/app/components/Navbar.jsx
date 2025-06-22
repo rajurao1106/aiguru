64166d922a45033b8113a5cfaf7c4a0a8ebf67b2
rajurao <rajurao1107@gmail.com>
2025-06-23 00:16:30 +0530

    Your changes

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,12 +8,12 @@ import { IoIosMoon } from "react-icons/io";
 export default function Navbar({ theme, themeHandle }) {
   return (
     <nav
-      className={`w-full z-50 h-16 ${
+      className={`w-full z-50 h-16  ${
         theme ? "bg-white duration-300 text-black shadow" : " bg-gray-900 duration-300 text-white"
       } 
  flex justify-center items-center`}
     >
-      <div className="w-full max-w-[1300px] flex justify-between items-center">
+      <div className="w-full max-w-[1300px] max-lg:w-[90%] flex justify-between items-center">
         <div className="flex gap-5">
           <div className="">
             <FiMenu size={30} />
