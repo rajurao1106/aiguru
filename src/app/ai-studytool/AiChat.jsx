46f0722023c98307b2bb9f6f1ddeda2a2fa47bfe
rajurao <rajurao1107@gmail.com>
2025-06-30 08:38:24 +0530

    commit changes

--- src/app/ai-studytool/AiChat.jsx
+++ src/app/ai-studytool/AiChat.jsx
@@ -55,11 +55,11 @@ export default function AiChat({
     <section className={`w-full h-full ${textTheme} flex flex-col justify-end`}>
       <div className="max-w-2xl mx-auto flex flex-col w-full">
         {/* Chat History */}
-        <div className="flex flex-col gap-3 overflow-y-scroll custom-scrollbar max-h-[27rem] px-6 py-4">
+        <div className="flex flex-col gap-3 overflow-y-scroll custom-scrollbar h-[27rem] max-lg:h-[76vh] px-6 py-4">
           {messages.map((msg, index) => (
             <div key={index} className="flex flex-col gap-2">
               <div
-                className={`p-3 rounded-xl text-sm w-fit max-w-[90%] ${
+                className={`p-3  rounded-xl text-sm w-fit max-w-[90%] ${
                   msg.role === "user"
                     ? "ml-auto bg-blue-600 text-white"
                     : theme
