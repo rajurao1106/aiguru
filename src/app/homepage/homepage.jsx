4b75b47c41206dd8c794b97398252b93564f5685
rajurao <rajurao1107@gmail.com>
2025-07-03 00:07:04 +0530

    commit changes

--- src/app/homepage/homepage.jsx
+++ src/app/homepage/homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import Features from './Features'
 
-export default function Homepage() {
+export default function homepage() {
   return (
     <div>
       <Hero/>
